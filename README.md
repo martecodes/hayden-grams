bf126fa4c2b06d63ae06cf093ae26bcc7c3b02ef
MarteCodes <jeanmarte93@gmail.com>
2021-09-12 11:31:17 -0400

    update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Hayden Grams
+# Hayden Grams (House Hayden)
 Instagram but better
 ![Screenshot 2021-11-06 194358](https://user-images.githubusercontent.com/88953222/140627125-ea4709e1-b200-4614-9e36-68bcc7ae3de2.png)
 
--- app/routes.js
+++ app/routes.js
@@ -1,69 +1,69 @@
-module.exports = function(app, passport, db, multer, ObjectId) {
+module.exports = function (app, passport, db, multer, ObjectId) {
 
- // Image Upload Code =========================================================================
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'public/images/uploads')
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.fieldname + '-' + Date.now() + ".png")
-  }
-});
-const upload = multer({storage: storage}); 
-
-// normal routes ===============================================================
-
-    // show the home page (will also have our login links)
-    app.get('/', function(req, res) {
-        res.render('index.ejs');
-    });
-
-    // PROFILE SECTION =========================
-    app.get('/profile', isLoggedIn, function(req, res) {
-      let user = req.user
-        db.collection('posts').find({postedBy: user.local.username}).toArray((err, result) => {
-          db.collection('comments').find().toArray((err, mainResult) => {
-
-            if (err) return console.log(err)
-            res.render('profile.ejs', {
-              user: req.user,
-              posts: result,
-              comments: mainResult
-            })
-          })
-        })
-    });
-
-    //new post page
-  app.get('/newpost', isLoggedIn, function (req, res) {
-      res.render('newpost.ejs', {
-        user: req.user
-      })
+  // Image Upload Code =========================================================================
+  const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'public/images/uploads')
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.fieldname + '-' + Date.now() + ".png")
+    }
   });
-    //feed page
-    app.get('/feed', function(req, res) {
-      db.collection('posts').find().toArray((err, result) => {
-        db.collection('comments').find().toArray((err, mainResult) => {
-          if (err) return console.log(err)
-          res.render('feed.ejs', {
-            user: req.user,
-            posts: result,
-            comments: mainResult
-          })
+  const upload = multer({ storage: storage });
+
+  // normal routes ===============================================================
+
+  // show the home page (will also have our login links)
+  app.get('/', function (req, res) {
+    res.render('index.ejs');
+  });
+
+  // PROFILE SECTION =========================
+  app.get('/profile', isLoggedIn, function (req, res) {
+    let user = req.user
+    db.collection('posts').find({ postedBy: user.local.username }).toArray((err, result) => {
+      db.collection('comments').find().toArray((err, mainResult) => {
+
+        if (err) return console.log(err)
+        res.render('profile.ejs', {
+          user: req.user,
+          posts: result,
+          comments: mainResult
         })
       })
-    });
+    })
+  });
+
+  //new post page
+  app.get('/newpost', isLoggedIn, function (req, res) {
+    res.render('newpost.ejs', {
+      user: req.user
+    })
+  });
+  //feed page
+  app.get('/feed', function (req, res) {
+    db.collection('posts').find().toArray((err, result) => {
+      db.collection('comments').find().toArray((err, mainResult) => {
+        if (err) return console.log(err)
+        res.render('feed.ejs', {
+          user: req.user,
+          posts: result,
+          comments: mainResult
+        })
+      })
+    })
+  });
   //post page
-  app.get('/post/:username/:postId', isLoggedIn, function(req, res) {
+  app.get('/post/:username/:postId', isLoggedIn, function (req, res) {
     let postId = ObjectId(req.params.postId)
-    
+
     db.collection('posts').find({
       _id: postId
     }).toArray((err, result) => {
       db.collection('comments').find({
         postId: req.params.postId
-      }).toArray((err, mainResult) =>{
-        
+      }).toArray((err, mainResult) => {
+
         if (err) return console.log(err)
         res.render('post.ejs', {
           posts: result,
@@ -74,55 +74,55 @@ const upload = multer({storage: storage});
     })
   });
 
-//profile page
-app.get('/page/:username', isLoggedIn, function(req, res) {
-  let user = req.params.username;
-  db.collection('posts').find({postedBy: user}).toArray((err, result) => {
-    db.collection('comments').find().toArray((err, mainResult) => {
-      if (err) return console.log(err)
-      res.render('page.ejs', {
-        user: req.user,
-        posts: result,
-        comments: mainResult
+  //profile page
+  app.get('/page/:username', isLoggedIn, function (req, res) {
+    let user = req.params.username;
+    db.collection('posts').find({ postedBy: user }).toArray((err, result) => {
+      db.collection('comments').find().toArray((err, mainResult) => {
+        if (err) return console.log(err)
+        res.render('page.ejs', {
+          user: req.user,
+          posts: result,
+          comments: mainResult
+        })
       })
     })
-  })
-});
+  });
 
-    // LOGOUT ==============================
-    app.get('/logout', function(req, res) {
-        req.logout();
-        res.redirect('/');
-    });
-// post routes
-app.post('/makePost', upload.single('file-to-upload'), (req, res) => {
-  let user = req.user;
+  // LOGOUT ==============================
+  app.get('/logout', function (req, res) {
+    req.logout();
+    res.redirect('/');
+  });
+  // post routes
+  app.post('/makePost', upload.single('file-to-upload'), (req, res) => {
+    let user = req.user;
 
-  db.collection('posts').insertOne({
-    caption: req.body.caption, 
-    img: 'images/uploads/' + req.file.filename, 
-    postedBy: user.local.username, 
-    likes: 0, 
-    liked: false,
-    commentsTotal: 0
-  }, (err, result) => {
-    if (err) return console.log(err)
-    res.redirect('/feed')
+    db.collection('posts').insertOne({
+      caption: req.body.caption,
+      img: 'images/uploads/' + req.file.filename,
+      postedBy: user.local.username,
+      likes: 0,
+      liked: false,
+      commentsTotal: 0
+    }, (err, result) => {
+      if (err) return console.log(err)
+      res.redirect('/feed')
+    })
   })
-})
 
   app.post('/comments/:postId', (req, res) => {
     let user = req.user;
     let time = (new Date()).toLocaleString();
     const postId = req.params.postId;
-    
+
     db.collection('comments').insertOne({
       commentBy: user.local.username,
       comment: req.body.comment,
       likes: 0,
-      liked : false,
+      liked: false,
       time,
-      postId : postId
+      postId: postId
     }, (err, result) => {
       if (err) return console.log(err)
       res.redirect('back')
@@ -130,12 +130,12 @@ app.post('/makePost', upload.single('file-to-upload'), (req, res) => {
   })
 
 
-// message board routes ===============================================================
+  // message board routes ===============================================================
 
-    app.put('/postLikes', (req, res) => {
-      
-      db.collection('posts')
-      .findOneAndUpdate({_id: ObjectId(req.body._id)}, {
+  app.put('/postLikes', (req, res) => {
+
+    db.collection('posts')
+      .findOneAndUpdate({ _id: ObjectId(req.body._id) }, {
         $inc: {
           likes: 1,
         },
@@ -143,14 +143,14 @@ app.post('/makePost', upload.single('file-to-upload'), (req, res) => {
           liked: true,
         }
       }, {
-        sort: {_id: -1},
+        sort: { _id: -1 },
         upsert: true
       }, (err, result) => {
         if (err) return res.send(err)
         res.send(result)
       })
-    });
-  
+  });
+
   app.put('/commentLikes', (req, res) => {
     db.collection('comments')
       .findOneAndUpdate({ _id: ObjectId(req.body._id) }, {
@@ -169,19 +169,19 @@ app.post('/makePost', upload.single('file-to-upload'), (req, res) => {
       })
   });
 
-    app.delete('/commentDelete', (req, res) => {
-    
-      db.collection('comments').findOneAndDelete({
-        _id: ObjectId(req.body._id), 
-        comment: req.body.comment
-      }, (err, result) => {
-        if (err) return res.send(500, err)
-        res.send(result)
-      })
-    });
+  app.delete('/commentDelete', (req, res) => {
+
+    db.collection('comments').findOneAndDelete({
+      _id: ObjectId(req.body._id),
+      comment: req.body.comment
+    }, (err, result) => {
+      if (err) return res.send(500, err)
+      res.send(result)
+    })
+  });
 
   app.delete('/postDelete', (req, res) => {
-    
+
     db.collection('posts').findOneAndDelete({
       _id: ObjectId(req.body._id),
     }, (err, result) => {
@@ -189,60 +189,60 @@ app.post('/makePost', upload.single('file-to-upload'), (req, res) => {
       res.send(result)
     })
   });
-// =============================================================================
-// AUTHENTICATE (FIRST LOGIN) ==================================================
-// =============================================================================
+  // =============================================================================
+  // AUTHENTICATE (FIRST LOGIN) ==================================================
+  // =============================================================================
 
-    // locally --------------------------------
-        // LOGIN ===============================
-        // show the login form
-        app.get('/login', function(req, res) {
-            res.render('login.ejs', { message: req.flash('loginMessage') });
-        });
+  // locally --------------------------------
+  // LOGIN ===============================
+  // show the login form
+  app.get('/login', function (req, res) {
+    res.render('login.ejs', { message: req.flash('loginMessage') });
+  });
 
-        // process the login form
-        app.post('/login', passport.authenticate('local-login', {
-            successRedirect : '/feed', // redirect to the secure profile section
-            failureRedirect : '/login', // redirect back to the signup page if there is an error
-            failureFlash : true // allow flash messages
-        }));
+  // process the login form
+  app.post('/login', passport.authenticate('local-login', {
+    successRedirect: '/feed', // redirect to the secure profile section
+    failureRedirect: '/login', // redirect back to the signup page if there is an error
+    failureFlash: true // allow flash messages
+  }));
 
-        // SIGNUP =================================
-        // show the signup form
-        app.get('/signup', function(req, res) {
-            res.render('signup.ejs', { message: req.flash('signupMessage') });
-        });
+  // SIGNUP =================================
+  // show the signup form
+  app.get('/signup', function (req, res) {
+    res.render('signup.ejs', { message: req.flash('signupMessage') });
+  });
 
-        // process the signup form
-        app.post('/signup', passport.authenticate('local-signup', {
-            successRedirect : '/profile', // redirect to the secure profile section
-            failureRedirect : '/signup', // redirect back to the signup page if there is an error
-            failureFlash : true // allow flash messages
-        }));
+  // process the signup form
+  app.post('/signup', passport.authenticate('local-signup', {
+    successRedirect: '/profile', // redirect to the secure profile section
+    failureRedirect: '/signup', // redirect back to the signup page if there is an error
+    failureFlash: true // allow flash messages
+  }));
 
-// =============================================================================
-// UNLINK ACCOUNTS =============================================================
-// =============================================================================
-// used to unlink accounts. for social accounts, just remove the token
-// for local account, remove email and password
-// user account will stay active in case they want to reconnect in the future
+  // =============================================================================
+  // UNLINK ACCOUNTS =============================================================
+  // =============================================================================
+  // used to unlink accounts. for social accounts, just remove the token
+  // for local account, remove email and password
+  // user account will stay active in case they want to reconnect in the future
 
-    // local -----------------------------------
-    app.get('/unlink/local', isLoggedIn, function(req, res) {
-        var user            = req.user;
-        user.local.email    = undefined;
-        user.local.password = undefined;
-        user.save(function(err) {
-            res.redirect('/profile');
-        });
+  // local -----------------------------------
+  app.get('/unlink/local', isLoggedIn, function (req, res) {
+    var user = req.user;
+    user.local.email = undefined;
+    user.local.password = undefined;
+    user.save(function (err) {
+      res.redirect('/profile');
     });
+  });
 
 };
 
 // route middleware to ensure user is logged in
 function isLoggedIn(req, res, next) {
-    if (req.isAuthenticated())
-        return next();
+  if (req.isAuthenticated())
+    return next();
 
-    res.redirect('/');
+  res.redirect('/');
 }
