c0e33eadc317c1297564966cfc7b562bcb0f4ff3
MarteCodes <jeanmarte93@gmail.com>
2021-11-06 19:48:51 -0400

    quick pound files

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url': 'mongodb+srv://Yoda:Password@cluster0.jt19a.mongodb.net/instagram?retryWrites=true&w=majority',
+    'dbName': 'instagram'
+};
